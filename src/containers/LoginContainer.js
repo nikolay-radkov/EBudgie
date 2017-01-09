35a3707d0e737e19312f6593f0f52b9ede72e83c
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-01-09 19:10:17 +0200

    Integrate Account Kit to the project

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import RNAccountKit from 'react-native-facebook-account-kit'
 
 import { push, pop } from '../actions/navigationActions';
 
 class LoginContainer extends Component {
+  constructor(state) {
+    super(state);
+    this.loginWithEmail = this.loginWithEmail.bind(this);
+    this.loginWithPhone = this.loginWithPhone.bind(this);
+  }
+
+  loginWithPhone() {
+    RNAccountKit.loginWithPhone()
+      .then((token) => {
+        if (!token) {
+          console.log('Login cancelled')
+        } else {
+          console.log(`Logged with phone. Token: ${token}`)
+          this.props.pushRoute({ key: 'home', title: 'Home' })
+        }
+      })
+
+  }
+
+  loginWithEmail() {
+    RNAccountKit.loginWithEmail()
+      .then((token) => {
+        if (!token) {
+          console.log('Login cancelled')
+        } else {
+          console.log(`Logged with email. Token: ${token}`)
+          this.props.pushRoute({ key: 'home', title: 'Home' })
+        }
+      });
+  }
 
   render() {
-    return <Text onPress={ () => this.props.pushRoute({ key: 'home', title: 'Home' })}> Login</Text>;
+    return <View>
+      <View>
+        <Text
+          onPress={this.loginWithEmail}>
+          Login with Email
+      </Text>
+      </View>
+      <View>
+        <Text onPress={this.loginWithPhone}>
+          Login with Phone
+        </Text>
+      </View>
+    </View>;
   }
 }
 
