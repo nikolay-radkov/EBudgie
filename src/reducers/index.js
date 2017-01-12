bd8c991dc3fe7e39a155c2461f25c3645bdee816
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-01-12 18:44:16 +0200

    Remove useless template code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import navigation from './navigation';
-import todo from './todo';
 import pouchdb from './pouchdb';
 import drawer from './drawer';
 
 const rootReducer = combineReducers({
   navigation,
-  todo,
   pouchdb,
   drawer
 });
