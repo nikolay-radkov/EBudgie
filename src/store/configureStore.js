bc057dad2ac79c360420c0fa19273576bf5f347c
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-01-14 18:35:51 +0200

    Forbid react-logger to log anything in production

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,46 +1,3 @@
-// import { createStore } from 'redux';
-
-
-
-// function todoStore(state = defaultState, action) {
-//   switch (action.type) {
-//     case 'ADD_TODO':
-//       const allTodos = state.allTodos.concat([{
-//         task: action.task,
-//         state: 'pending'
-//       }]);
-//       return Object.assign({}, state, {
-//         allTodos,
-//         todos: allTodos.filter(todo => todo.state === state.filter)
-//       });
-//     case 'DONE_TODO':
-//       const doneTodo =  Object.assign({}, action.todo,{
-//         state: 'done'
-//       });
-
-//       const updatedAllTodos = state.allTodos.map(todo => {
-//         return todo === action.todo ? doneTodo : todo;
-//       });
-
-//       return Object.assign({}, state, {
-//         allTodos: updatedAllTodos,
-//         todos: updatedAllTodos.filter(todo => todo.state === state.filter)
-//       });
-//     case 'TOGGLE_STATE':
-//       const filter = state.filter === 'pending' ? 'done' : 'pending';
-//       return Object.assign({}, state, {
-//         filter,
-//         todos: state.allTodos.filter(todo => todo.state === filter)
-//       });
-//     default:
-//       return state;
-//   }
-// }
-// const store = createStore(todoStore);
-
-// export default store;
-
-
 import {
   createStore,
   applyMiddleware
@@ -52,7 +9,10 @@ import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-  const logger = createLogger();
+
+  const logger = createLogger({
+      predicate: (getState, action) => __DEV__
+  });
   const middlewares = [
     thunk,
     logger
