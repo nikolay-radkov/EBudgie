af34f2183d2731bf651ec5bb57f16b2dafc36166
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-01-14 22:23:56 +0200

    Add action creators for addCategoryForm

---
+++ src/actionCreators/addCategoryForm.js
@@ -0,0 +1,62 @@
+import {
+  SET_CATEGORY_TITLE,
+  SET_CATEGORY_COLOR,
+  SET_CATEGORY_ICON,
+  OPEN_COLOR_PICKER,
+  CLOSE_COLOR_PICKER,
+  OPEN_ICON_PICKER,
+  CLOSE_ICON_PICKER,
+  SET_OFFSET
+} from '../constants/ActionTypes';
+
+export const setCategoryTitle = (title) => {
+  return {
+    type: SET_CATEGORY_TITLE,
+    title
+  };
+};
+
+export const setCategoryColor = (color) => {
+  return {
+    type: SET_CATEGORY_COLOR,
+    color
+  };
+};
+
+export const setCategoryIcon = (icon) => {
+  return {
+    type: SET_CATEGORY_ICON,
+    icon
+  };
+};
+
+export const openColorPicker = () => {
+  return {
+    type: OPEN_COLOR_PICKER
+  };
+};
+
+export const closeColorPicker = () => {
+  return {
+    type: CLOSE_COLOR_PICKER
+  };
+};
+
+export const openIconPicker = () => {
+  return {
+    type: OPEN_ICON_PICKER
+  };
+};
+
+export const closeIconPicker = () => {
+  return {
+    type: CLOSE_ICON_PICKER
+  };
+};
+
+export const setOffset = (offset) => {
+  return {
+    type: SET_OFFSET,
+    offset
+  };
+};
