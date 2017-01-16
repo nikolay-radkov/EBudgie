b8472691395f13bc1faa2ff642520ce912108532
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-01-17 00:10:36 +0200

    Add category reducer which will handle the changes in user categories

---
+++ src/reducers/category.js
@@ -0,0 +1,25 @@
+import { NEW_CATEGORY } from '../constants/ActionTypes';
+
+
+const initialState = {
+  types: []
+};
+
+export default (state = initialState, action) => {
+  const {
+    types
+  } = state;
+
+  switch (action.type) {
+    case NEW_CATEGORY:
+      return {
+        ...state,
+        types: [
+          ...types,
+          action.category
+        ]
+      };
+    default:
+      return state;
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import navigation from './navigation';
 import pouchdb from './pouchdb';
 import drawer from './drawer';
 import addCategoryForm from './addCategoryForm';
+import category from './category';
 
 const rootReducer = combineReducers({
   navigation,
   pouchdb,
   drawer,
-  addCategoryForm
+  addCategoryForm,
+  category
 });
 
 export default rootReducer;
