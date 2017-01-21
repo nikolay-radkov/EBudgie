9855fec865a335ae32af6c9341e2ef446c1009b9
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-01-21 17:35:23 +0200

    Rewrite the navigation reducer to use NavigationStateUtils

--- src/reducers/navigation.js
+++ src/reducers/navigation.js
@@ -1,8 +1,12 @@
-import { PUSH_ROUTE, POP_ROUTE } from '../constants/ActionTypes';
+import { PUSH_ROUTE, POP_ROUTE, REPLACE_ROUTE } from '../constants/ActionTypes';
+import { NavigationExperimental } from 'react-native';
+
+const {
+  StateUtils: NavigationStateUtils,
+} = NavigationExperimental;
 
 const initialState = {
   index: 0,
-  key: 'root',
   routes: [
     {
       key: 'login',
@@ -12,26 +16,13 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-  const {
-    index,
-    routes,
-  } = state;
   switch (action.type) {
     case PUSH_ROUTE:
-      return {
-        ...state,
-        routes: [
-          ...routes,
-          action.route,
-        ],
-        index: index + 1,
-      };
+      return NavigationStateUtils.push(state, action.route);
     case POP_ROUTE:
-      return index > 0 ? {
-        ...state,
-        routes: routes.slice(0, routes.length - 1),
-        index: index - 1,
-      } : state;
+      return NavigationStateUtils.pop(state);
+    case REPLACE_ROUTE:
+      return NavigationStateUtils.replaceAtIndex(state, state.index, action.route);
     default:
       return state;
   }
