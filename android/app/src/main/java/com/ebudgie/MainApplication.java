0760e246f3f0ca8507d65f10320b0fe4e0f5e3a5
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-01-22 16:53:56 +0200

    Add react-native-device-info to the project

--- android/app/src/main/java/com/ebudgie/MainApplication.java
+++ android/app/src/main/java/com/ebudgie/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import io.underscope.react.fbak.RNAccountKitPackage;
 import com.cboy.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
 
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNAccountKitPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage()
