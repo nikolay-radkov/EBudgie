a9d2fe49be56d49cb7eab6e7b94f63414a2fca0f
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-01-29 21:52:28 +0200

    Add react-native-push-notifications to the project

--- android/app/src/main/java/com/ebudgie/MainApplication.java
+++ android/app/src/main/java/com/ebudgie/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.reactlibrary.RNUUIDGeneratorPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import io.underscope.react.fbak.RNAccountKitPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
 
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNUUIDGeneratorPackage(),
             new RNDeviceInfo(),
             new RNAccountKitPackage(),
