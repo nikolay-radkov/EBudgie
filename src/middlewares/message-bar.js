abc74a376359bd50a769c15bd872152e7f71de55
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-05-08 20:04:50 +0300

    Add message bar alert to notification center

--- src/middlewares/message-bar.js
+++ src/middlewares/message-bar.js
@@ -4,6 +4,9 @@ import { MessageBarManager } from 'react-native-message-bar';
 import { StatusBar } from 'react-native';
 import { Icon } from 'react-native-elements';
 
+import { createNotification } from '../services/localNotifications';
+import { setNotificationIsSeen } from '../actionCreators/notifications';
+
 import {
   NEW_ITEM,
   NEW_CATEGORY,
@@ -21,71 +24,251 @@ import {
   NEW_THRESHOLD,
 } from '../constants/ActionTypes';
 
+const ICON_SIZE = 40;
+
 const messageBar = store => next => async action => {
   const result = next(action);
   let shouldShowMessage = true;
   const options = {
-    title: 'Your alert title goes here',
-    alertType: 'success',
     viewTopInset: StatusBar.currentHeight,
-    avatar: (
-      <Icon
-        color="white"
-        name="warning"
-        size={50}
-      />
-    )
+  };
+  const notificationOptions = {
+    isSeen: false,
   };
 
   switch (action.type) {
-    case NEW_ITEM:
-      options.title = 'New item added';
-      break;
-    case NEW_CATEGORY:
-      options.title = 'New category added';
-      break;
-    case EDIT_SALARY:
-      options.title = 'Salary updated';
-      break;
+    // ADD NOTIFICATIONS
     case NEW_INCOME:
-      options.title = 'New income added';
+      options.title = i18n.t('ELEMENT_ADDED');
+      options.message = i18n.t('NEW_INCOME_ADDED');
+      options.alertType = 'success';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="attach-money"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'NEW_INCOME_ADDED';
+      notificationOptions.icon = 'attach-money';
+      notificationOptions.route = 'calendar';
       break;
     case NEW_EXPENSE:
-      options.title = 'New expense added';
+      options.title = i18n.t('ELEMENT_ADDED');
+      options.message = i18n.t('NEW_EXPENSE_ADDED');
+      options.alertType = 'success';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="money-off"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'NEW_EXPENSE_ADDED';
+      notificationOptions.icon = 'money-off';
+      notificationOptions.route = 'calendar';
+      break;
+    case NEW_CATEGORY:
+      options.title = i18n.t('ELEMENT_ADDED');
+      options.message = i18n.t('NEW_CATEGORY_ADDED');
+      options.alertType = 'success';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="library-add"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'NEW_CATEGORY_ADDED';
+      notificationOptions.icon = 'library-add';
+      notificationOptions.route = 'categories';
+      break;
+    case NEW_ITEM:
+      options.title = i18n.t('ELEMENT_ADDED');
+      options.message = i18n.t('NEW_ITEM_ADDED');
+      options.alertType = 'success';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="add"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'NEW_ITEM_ADDED';
+      notificationOptions.icon = 'add';
+      notificationOptions.route = 'items';
       break;
     case NEW_THRESHOLD:
-      options.title = 'New threshold added';
+      options.title = i18n.t('ELEMENT_ADDED');
+      options.message = i18n.t('NEW_THRESHOLD_ADDED');
+      options.alertType = 'success';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="lock"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'NEW_THRESHOLD_ADDED';
+      notificationOptions.icon = 'lock';
       break;
+    // EDIT NOTIFICATIONS
     case EDIT_INCOME:
-      options.title = 'Income updated';
+      options.title = i18n.t('ELEMENT_UPDATED');
+      options.message = i18n.t('INCOME_UPDATED');
+      options.alertType = 'info';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="attach-money"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'INCOME_UPDATED';
+      notificationOptions.icon = 'attach-money';
+      notificationOptions.route = 'calendar';
       break;
     case EDIT_EXPENSE:
-      options.title = 'Expense updated';
+      options.title = i18n.t('ELEMENT_UPDATED');
+      options.message = i18n.t('EXPENSE_UPDATED');
+      options.alertType = 'info';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="money-off"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'EXPENSE_UPDATED';
+      notificationOptions.icon = 'money-off';
+      notificationOptions.route = 'calendar';
       break;
     case EDIT_CATEGORY:
-      options.title = 'Category updated';
+      options.title = i18n.t('ELEMENT_UPDATED');
+      options.message = i18n.t('CATEGORY_UPDATED');
+      options.alertType = 'info';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="library-add"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'CATEGORY_UPDATED';
+      notificationOptions.icon = 'library-add';
+      notificationOptions.route = 'categories';
       break;
     case EDIT_ITEM:
-      options.title = 'Item updated';
+      options.title = i18n.t('ELEMENT_UPDATED');
+      options.message = i18n.t('ITEM_UPDATED');
+      options.alertType = 'info';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="add"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'ITEM_UPDATED';
+      notificationOptions.icon = 'add';
+      notificationOptions.route = 'items';
       break;
+    case EDIT_SALARY:
+      options.title = i18n.t('ELEMENT_UPDATED');
+      options.message = i18n.t('SALARY_UPDATED');
+      options.alertType = 'info';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="repeat"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'SALARY_UPDATED';
+      notificationOptions.icon = 'repeat';
+      break;
+    // DELETE NOTIFICATIONS
     case DELETE_INCOME:
-      options.title = 'Income deleted';
+      options.title = i18n.t('ELEMENT_DELETED');
+      options.message = i18n.t('INCOME_DELETED');
+      options.alertType = 'error';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="attach-money"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'INCOME_DELETED';
+      notificationOptions.icon = 'attach-money';
+      notificationOptions.route = 'calendar';
       break;
     case DELETE_EXPENSE:
-      options.title = 'Expense deleted';
+      options.title = i18n.t('ELEMENT_DELETED');
+      options.message = i18n.t('EXPENSE_DELETED');
+      options.alertType = 'error';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="money-off"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'EXPENSE_DELETED';
+      notificationOptions.icon = 'money-off';
+      notificationOptions.route = 'calendar';
       break;
     case DELETE_CATEGORY:
-      options.title = 'Category deleted';
+      options.title = i18n.t('ELEMENT_DELETED');
+      options.message = i18n.t('CATEGORY_DELETED');
+      options.alertType = 'error';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="library-add"
+          size={ICON_SIZE}
+        />
+      );
+
+      notificationOptions.message = 'CATEGORY_DELETED';
+      notificationOptions.icon = 'library-add';
+      notificationOptions.route = 'categories';
       break;
     case DELETE_ITEM:
-      options.title = 'Item deleted';
-      break;
+      options.title = i18n.t('ELEMENT_DELETED');
+      options.message = i18n.t('ITEM_DELETED');
+      options.alertType = 'error';
+      options.avatar = (
+        <Icon
+          color="white"
+          name="add"
+          size={ICON_SIZE}
+        />
+      );
 
+      notificationOptions.message = 'ITEM_DELETED';
+      notificationOptions.icon = 'add';
+      notificationOptions.route = 'items';
+      break;
     default:
       shouldShowMessage = false;
   }
 
   if (shouldShowMessage) {
+    const notificationId = await createNotification(notificationOptions);
+    options.onTapped = () => store.dispatch(setNotificationIsSeen(notificationId, true));
     MessageBarManager.showAlert(options);
   }
 
