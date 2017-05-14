1dcb1b4c755687c1e7e6048c5bfa4bcb7dcac466
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-05-14 22:04:55 +0300

    Fix pouchdb bound action creator for new instance

--- src/boundActionCreators/pouchdb.js
+++ src/boundActionCreators/pouchdb.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
 import { newPouchDB } from '../actionCreators/pouchdb';
@@ -5,21 +6,27 @@ import { loadEBudgie } from '../actionCreators/ebudgie';
 import createPouchDB from '../config/pouchdb';
 import * as pouchdbService from '../services/pouchdbService';
 
-export const createNewPouchDB = (dbName) => {
+export const createNewPouchDB = (dbName, id) => {
   return async (dispatch) => {
-    const instance = createPouchDB(dbName);
-    let uuid = dbName;
+    let docId = id;
 
-    if (uuid === 'unauthorized') {
-      uuid = DeviceInfo.getUniqueID();
+    if (!id) {
+      if (dbName === 'unauthorized') {
+        docId = DeviceInfo.getUniqueID();
+      } else {
+        docId = dbName;
+      }
     }
 
-    dispatch(newPouchDB(instance, uuid, uuid));
+    const instance = createPouchDB(dbName, docId);
+    dispatch(newPouchDB(instance, dbName, docId));
+
+    await AsyncStorage.setItem('docId', docId);
+    await AsyncStorage.setItem('dbName', dbName);
 
     try {
-      const ebudgie = await pouchdbService.getDocument(uuid);
+      const ebudgie = await pouchdbService.getDocument(docId);
       dispatch(loadEBudgie(ebudgie));
-
       return Promise.resolve(ebudgie);
     } catch (e) {
       return Promise.resolve();
