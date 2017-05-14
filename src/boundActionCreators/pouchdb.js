f0d8ce061690563f7bef0c40eb3b99372123f593
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-05-15 01:26:25 +0300

    Fix pouchdb bound action creator

--- src/boundActionCreators/pouchdb.js
+++ src/boundActionCreators/pouchdb.js
@@ -3,8 +3,11 @@ import DeviceInfo from 'react-native-device-info';
 
 import { newPouchDB } from '../actionCreators/pouchdb';
 import { loadEBudgie } from '../actionCreators/ebudgie';
-import createPouchDB from '../config/pouchdb';
-import * as pouchdbService from '../services/pouchdbService';
+import {
+  createPouchDB,
+  syncDocument,
+  getDocument
+} from '../services/pouchdbService';
 
 export const createNewPouchDB = (dbName, id) => {
   return async (dispatch) => {
@@ -20,12 +23,12 @@ export const createNewPouchDB = (dbName, id) => {
 
     const instance = createPouchDB(dbName, docId);
     dispatch(newPouchDB(instance, dbName, docId));
-
+    syncDocument();
     await AsyncStorage.setItem('docId', docId);
     await AsyncStorage.setItem('dbName', dbName);
 
     try {
-      const ebudgie = await pouchdbService.getDocument(docId);
+      const ebudgie = await getDocument(docId);
       dispatch(loadEBudgie(ebudgie));
       return Promise.resolve(ebudgie);
     } catch (e) {
